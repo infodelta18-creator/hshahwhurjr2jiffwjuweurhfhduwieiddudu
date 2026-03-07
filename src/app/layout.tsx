0cb8649fd24e04dcad3017e02b3edc5e62077f09
infodelta18-creator <infodelta18@gmail.com>
2026-03-07 10:11:02 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Marg/'ilon shahar hokimligi',
-  description: 'Marg/'ilon shahar hokimligining rasmiy sayti',
+  title: 'Margilan shahar hokimligi',
+  description: 'Margilan shahar hokimligining rasmiy sayti',
 };
 
 export default function RootLayout({
