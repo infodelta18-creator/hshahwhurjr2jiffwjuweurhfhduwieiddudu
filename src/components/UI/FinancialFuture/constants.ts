46def69e76997d35edada4136e333b7367b7db4a
infodelta18-creator <infodelta18@gmail.com>
2026-03-08 15:01:51 +0500

    Update constants.ts

--- src/components/UI/FinancialFuture/constants.ts
+++ src/components/UI/FinancialFuture/constants.ts
@@ -2,26 +2,27 @@ import ic_money_send from '../../../../public/svgs/ic_money_send.svg';
 import ic_wallet_minus from '../../../../public/svgs/ic_wallet_minus.svg';
 export const cardsInfo = [
   {
-    title: 'Spend Better',
+    title: 'Turizm rivojlanishi',
     details:
-      'Set and achieve financial goals with ease, automate your savings, and watch your money grow without the stress.',
+      `Margʻilon shahri turizm uchun ham qiziqarli hudud hisoblanadi. Shaharga kelgan sayyohlar asosan ipakchilik jarayoni va hunarmandchilik bilan tanishadilar.
+Shaharda har yili Atlas Bayrami festivali o‘tkaziladi. Bu festival milliy matolar va hunarmandchilikni namoyish etadi.`,
     icon: ic_money_send,
   },
   {
-    title: 'Invest Better',
+    title: `Margʻilon dagi investitsiyalar`,
     details:
-      'Set and achieve financial goals with ease, automate your savings, and watch your money grow without the stress.',
+      `So‘nggi yillarda Margʻilon shahri iqtisodiyotini rivojlantirish uchun mahalliy va xorijiy investitsiyalar jalb qilinmoqda. Bu investitsiyalar asosan sanoat, turizm, savdo, qurilish va qishloq xo‘jaligi sohalariga yo‘naltirilgan.`, `Jumladan: Margilan City Mall loyihasi.Shaharda zamonaviy savdo va turar-joy majmuasi qurilmoqda.Loyihaning umumiy qiymati 35 million dollar.Majmuada: savdo markazi, ko‘ngilochar hudud, 11 ta ko‘p qavatli uy, 785 ta xonadon avtoturargoh va bolalar maydonchalari bo‘ladi.`,
     icon: ic_wallet_minus,
   },
 ];
 
 export const stats = [
   {
-    number: '50+',
+    number: '60+',
     subtitle: 'Mahalla',
   },
   {
-    number: '50,000+',
+    number: '4000+',
     subtitle: 'Tadbirkorlar',
   },
   {
@@ -36,18 +37,16 @@ export const stats = [
 
 // For desktop
 export const desktopHeaderPhrase = [
-  `Marg'ilon shahar`,
-  `qishloq xo'jaligi`,
+  `Marg'ilon shahri haqida`,
 ];
 export const desktopParagraphPhrase = [
-  'At RAFT, we empower you to confidently shape your financial future. Our modern',
-  'approach simplifies saving and investing, making it easier than ever.',
+  `Margʻilon — Oʻzbekistonning Fargʻona vodiysi dagi eng qadimiy shaharlardan biri. U Fargʻona viloyatida joylashgan va tarixan ipakchilik hamda hunarmandchiligi bilan mashhur.`,
 ];
 
 // For mobile
 export const mobileHeaderPhrase = [
- `Marg'ilon shahar`,
-  `qishloq xo'jaligi`,
+ `Marg'ilon shahri`,
+  `haqida`,
 ];
 export const mobileParagraphPhrase = [
   'At RAFT, we empower you to confidently shape',
