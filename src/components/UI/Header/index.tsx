faa962e10e850c856ce9ccf9ee56d13ed50fb64f
infodelta18-creator <infodelta18@gmail.com>
2026-03-08 08:36:51 +0500

    Update index.tsx

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,52 +1,49 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
-import { motion } from 'framer-motion';
 import {
   Wrapper,
   Inner,
   LogoContainer,
   Nav,
   CallToActions,
+  AbsoluteLinks,
   BurgerMenu,
 } from './styles';
 import raft_logo from '../../../../public/svgs/margilanhokimligi.jpg';
 import ic_bars from '../../../../public/svgs/ic_bars.svg';
 import { GetStartedButton } from '@/components';
 import AnimatedLink from '@/components/Common/AnimatedLink';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { links, menu } from './constants';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   return (
     <Wrapper>
       <Inner>
         <LogoContainer>
-          <Image
-            src={raft_logo}
-            alt="Marg‘ilon hokimligi logo"
-            width={50}
-            height={50}
-            priority
-          />
+         <Image 
+  src={raft_logo} 
+  alt="raft_logo" 
+  priority 
+  style={{ width: "60px", height: "60px" }} 
+/>} alt="raft_logo" priority />
           <BurgerMenu onClick={() => setIsOpen(!isOpen)}>
             <motion.div
               variants={menu}
               animate={isOpen ? 'open' : 'closed'}
               initial="closed"
-            />
-            <Image src={ic_bars} alt="menu bars" width={24} height={24} />
+            ></motion.div>
+            <Image src={ic_bars} alt="bars" />
           </BurgerMenu>
         </LogoContainer>
-
         <Nav className={isOpen ? 'active' : ''}>
           {links.map((link, i) => (
             <AnimatedLink key={i} title={link.linkTo} />
           ))}
         </Nav>
-
         <CallToActions className={isOpen ? 'active' : ''}>
           <AnimatedLink title="Login" />
           <GetStartedButton padding="0.5rem 0.75rem" />
