9841432c6f58d79a2139367394a48911222bcf57
infodelta18-creator <infodelta18@gmail.com>
2026-03-08 08:21:23 +0500

    Update styles.ts

--- src/components/UI/Header/styles.ts
+++ src/components/UI/Header/styles.ts
@@ -1,100 +1,94 @@
 'use client';
-import Link from 'next/link';
 import { styled } from 'styled-components';
 
 export const Wrapper = styled.section`
   padding: 1rem 0;
   border-bottom: 0.5px solid #3d3d3d;
-
-  @media (max-width: 768px) {
-    padding: 0.75rem 0;
-  }
 `;
 
 export const Inner = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 90%;
+  width: 100%;
   max-width: 1440px;
   margin: 0 auto;
+  padding: 0 20px;
 `;
 
 export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  img {
+    width: 50px;
+    height: 50px;
+    object-fit: contain;
+  }
+
   @media (max-width: 768px) {
-    display: flex;
-    align-items: center;
     justify-content: space-between;
     width: 100%;
-
   }
 `;
 
 export const BurgerMenu = styled.div`
   display: none;
-  position: relative;
+  cursor: pointer;
 
   @media (max-width: 768px) {
     display: block;
-    padding: 0.5rem;
+  }
 
-    div {
-      position: absolute;
-      background: var(--emerald);
-      width: '250px';
-      height: '300px';
-      border-radius: 25px;
-      z-index: 1;
-      top: 50px;
-    }
+  div {
+    position: absolute;
+    top: 60px;
+    right: 50px;
+    width: 250px;
+    height: 300px;
+    border-radius: 20px;
+    background: var(--emerald);
+    z-index: 2;
+  }
 
-    img {
-      position: relative;
-      z-index: 2;
-      object-fit: cover;
-    }
+  img {
+    position: relative;
+    z-index: 3;
   }
 `;
 
-export const Nav = styled.div`
+export const Nav = styled.nav`
   display: flex;
   align-items: center;
-  gap: 3.75rem;
-  position: relative;
-  margin-right: -6.3rem;
+  gap: 3rem;
 
   a {
     color: var(--link-color);
     font-size: 1rem;
     font-weight: 400;
+    text-decoration: none;
   }
 
   @media (max-width: 768px) {
     position: absolute;
     top: 60px;
+    right: 20px;
     flex-direction: column;
-    gap: 1rem;
     align-items: flex-start;
-    right: 120px;
-    z-index: 3;
+    gap: 1rem;
+    background: #111;
+    padding: 1rem 2rem;
+    border-radius: 10px;
     visibility: hidden;
     opacity: 0;
-    transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    transition-delay: 0.5s;
+    transition: all 0.3s ease;
 
     &.active {
-      opacity: 1;
       visibility: visible;
+      opacity: 1;
     }
   }
-`;
-
-export const AbsoluteLinks = styled(Link)`
-  position: absolute;
-  top: 40px;
-  color: var(--link-color);
-  font-size: 1rem;
-  font-weight: 400;
 `;
 
 export const CallToActions = styled.div`
@@ -102,27 +96,25 @@ export const CallToActions = styled.div`
   align-items: center;
   gap: 1rem;
 
-  div {
-    span {
-      color: var(--white);
-      font-size: 1rem;
-      font-weight: 600;
-    }
+  div span {
+    color: var(--white);
+    font-size: 1rem;
+    font-weight: 600;
   }
 
   @media (max-width: 768px) {
     position: absolute;
-    top: 220px;
-    z-index: 3;
+    top: 380px;
     right: 50px;
+    flex-direction: column;
+    gap: 1rem;
     visibility: hidden;
     opacity: 0;
-    transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    transition-delay: 0.5s;
+    transition: all 0.3s ease;
 
     &.active {
-      opacity: 1;
       visibility: visible;
+      opacity: 1;
     }
   }
 `;
